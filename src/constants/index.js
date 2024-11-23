d4222b9a19e059b6af9da53f5dcf75d0cc0474a8
yashmaheshwari0502 <yashmaheshwari0502@gmail.com>
2024-11-23 10:48:51 +0530

    complete website

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "New Delhi , India",
+  phoneNo: "+91 9650700225",
+  email: "yashmaheshwari0502@gmail.com",
 };
