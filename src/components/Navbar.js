f951e1b729215d7c3a327221675f30c9ce910221
yashmaheshwari0502 <yashmaheshwari0502@gmail.com>
2024-11-24 11:52:52 +0530

    complete websit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink=0 items-center">
-        <img src={logo} alt="Logo" className="mx-2 w-20" />
+        <img src={logo} alt="Logo" className="mx-2 w-72" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href="https://www.linkedin.com/in/yash-maheshwari-903294214/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
